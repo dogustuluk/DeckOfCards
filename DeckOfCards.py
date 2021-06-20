f6d316f26f00ca7c9a198201994d7bb7cd338f9c
dogustuluk <dogus.tuluk@gmail.com>
2021-06-20 16:23:29 +0300

    FirstCommit

---
+++ DeckOfCards.py
@@ -0,0 +1,70 @@
+print("Deck Of Cards")
+
+import random
+class Card(object):
+    def __init__(self, suit, val):
+        self.suit = suit
+        self.value=val
+    
+    def show(self):
+        print ("{} of {}".format(self.value, self.suit)   )
+       
+        
+        
+
+class Deck(object):
+    def __init__(self):
+        self.cards = []
+        self.build()
+        
+    def build(self):
+        for s in ["spades","clubs","diamonds","hearts"]:
+            for v in range(1, 14):
+                self.cards.append(Card(s,v))
+                #print ("{} of {}".format(v,s))
+    
+    def show(self):
+        for c in self.cards:
+            c.show()
+
+    def shuffle(self):
+        for i in range(len(self.cards)-1, 0, -1):
+            r = random.randint(0, i)
+            self.cards[i], self.cards[r] = self.cards[r], self.cards[i]
+            
+    def drawCard(self):
+        return self.cards.pop()
+
+class Player(object):
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+        
+    def draw(self, deck):
+        self.hand.append(deck.drawCard())
+        return self
+    
+    def showHand(self):
+        for card in self.hand:
+            card.show()
+        
+    def discard(self):
+        return self.hand.pop()    
+        
+        
+#card = Card("Clubs",6)
+#card.show()
+
+
+deck= Deck()
+#deck.show()
+deck.shuffle()
+deck.show()
+
+# Dogus= Player("Dogus")
+# Dogus.draw(deck).draw(deck)
+# Dogus.showHand()
+
+
+#card= deck.draw()
+#card.show()
